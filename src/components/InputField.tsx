cd4c0e34039adfeb69fd74b80eebe4288c9a794f
thuccv98 <thuccv98@gmail.com>
2021-09-06 16:41:52 +0700

    Pass props to component in typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface IProps {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: IProps) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
