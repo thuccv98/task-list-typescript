403a4c7e3dc0af579f4679cf5d202c1398374b4d
thuccv98 <thuccv98@gmail.com>
2021-09-06 16:21:36 +0700

    Create input ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <span className="heading">Task-List</span>
+      <InputField />
     </div>
   );
 };
